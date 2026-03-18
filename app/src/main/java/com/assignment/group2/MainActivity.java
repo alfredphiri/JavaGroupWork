4b9bd16a95bc9a1e27dff1441a51971f6d49dd5f
alf <alfredjbphiri@gmail.com>
2026-03-18 07:05:49 +0200

    Fix layout constraints and add scrolling to signup form

--- app/src/main/java/com/assignment/group2/MainActivity.java
+++ app/src/main/java/com/assignment/group2/MainActivity.java
@@ -1,9 +1,11 @@
 package com.assignment.group2;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-
+import android.view.View;
+import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
